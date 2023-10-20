6934108975fb1d5a36dda02501790b7723ec310a
Pedro Ivo Botter Ribas <pedro.ribas@bompracredito.com.br>
2023-10-20 14:39:32 -0400

    feat Create CommandComponent

--- src/app/market-list/components/view-market-list/components/command/command.component.ts
+++ src/app/market-list/components/view-market-list/components/command/command.component.ts
@@ -1,11 +1,13 @@
 import { Component } from "@angular/core";
 import { ViewMarketListService } from "../../view-market-list.service";
+import { faFileArrowDown } from "@fortawesome/free-solid-svg-icons";
 
 @Component({
   selector: 'app-command',
   templateUrl: './command.component.html'
 })
 export class CommandComponent {
+  public faFileArrowDown = faFileArrowDown;
   public isAddProductModalOpen = false;
 
   constructor(
