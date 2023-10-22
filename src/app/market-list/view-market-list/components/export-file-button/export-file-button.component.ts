fa6053900d079d0f33f123fe298a27742ff1833d
Pedro Ivo Botter Ribas <pedro.ribas@bompracredito.com.br>
2023-10-22 10:13:33 -0400

    feat Export products from view

--- src/app/market-list/view-market-list/components/export-file-button/export-file-button.component.ts
+++ src/app/market-list/view-market-list/components/export-file-button/export-file-button.component.ts
@@ -1,38 +1,79 @@
-import { Component } from "@angular/core";
+import { Component, Input, OnInit } from "@angular/core";
 import { Subscription } from "rxjs";
 import { AlertService } from "src/app/components/alert/alert.service";
 import { MarketListStore } from "src/app/market-list/shared/stores/market-list.store";
+import { ViewedProductsStore } from "src/app/market-list/shared/stores/viewed-products.store";
+import { Product } from "src/app/shared/models/product";
 import { ExportFileService } from "src/app/shared/services/export-file.service";
 
 @Component({
   selector: 'app-export-file-button',
   templateUrl: './export-file-button.component.html'
 })
-export class ExportFileButtonComponent {
-  private marketListSubscription!: Subscription;
+export class ExportFileButtonComponent implements OnInit {
+  protected buttonTitle = 'Exportar';
+
+  @Input()
+    public exportProductsFrom: 'default' | 'view' = 'default';
 
   constructor(
     private marketListStore: MarketListStore,
+    private viewedProductsStore: ViewedProductsStore,
     private alertService: AlertService,
     private exportFileService: ExportFileService
   ) { }
 
-  public exportFile() {
-    this.marketListSubscription = this.marketListStore.load()
-      .subscribe((marketList) => {
-        if (marketList.products  === null) {
-          return;  
-        }
-        try {
-          this.exportFileService
-            .convertProductsToCsv(marketList.products)
-            .toBlobUTF8_BOM()
-            .download();
-        } catch (err: any) {
-          this.alertService.error(err);
-        }
-      });
+  ngOnInit(): void {
+    switch (this.exportProductsFrom) {
+      case 'view':
+        this.buttonTitle = 'Exportar tela';
+        break;
+      default:
+        break;
+    }
+  }
 
-    this.marketListSubscription.unsubscribe();
+  public exportFile() {
+    let products: Product[];
+
+    switch (this.exportProductsFrom) {
+      case 'view':
+        products = this.getViewedProducts();        
+        break;
+      default:
+        products = this.getMarketListProducts();
+        break;
+    }
+
+    try {
+      this.exportFileService
+        .convertProductsToCsv(products)
+        .toBlobUTF8_BOM()
+        .download();
+    } catch (err: any) {
+      this.alertService.error(err);
+    }
+  }
+
+  private getMarketListProducts(): Product[] {
+    let products: Product[] = [];
+    
+    this.marketListStore
+      .load()
+      .subscribe((l) => products = l.products)
+      .unsubscribe();
+    
+    return products;
+  }
+
+  private getViewedProducts(): Product[] {
+    let products: Product[] = [];
+
+    this.viewedProductsStore
+      .load()
+      .subscribe((p) => products = p)
+      .unsubscribe();
+    
+    return products;
   }
 }
